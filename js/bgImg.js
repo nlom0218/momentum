cfa6545ec0a40f36a181f97a149a518c1acf3a44
KHD <nlom0218@naver.com>
2020-11-25 23:36:36 +0900

    remove background image
    remove background image

--- js/bgImg.js
+++ js/bgImg.js
@@ -1,22 +1,22 @@
 const $body = document.querySelector("body");
 
-const IMG_NUM = 6;
+// const IMG_NUM = 6;
 
-setBgImg = (num) => {
-  const img = document.createElement("img");
-  img.src = `./img/scenery/${num}.jpg`;
-  img.classList.add("bgImg");
-  $body.appendChild(img);
-};
+// setBgImg = (num) => {
+//   const img = document.createElement("img");
+//   img.src = `./img/scenery/${num}.jpg`;
+//   img.classList.add("bgImg");
+//   $body.appendChild(img);
+// };
 
-setImgNumber = () => {
-  const number = Math.floor(Math.random() * IMG_NUM + 1);
-  return number;
-};
+// setImgNumber = () => {
+//   const number = Math.floor(Math.random() * IMG_NUM + 1);
+//   return number;
+// };
 
-function init() {
-  const imgNumber = setImgNumber();
-  setBgImg(imgNumber);
-}
+// function init() {
+//   const imgNumber = setImgNumber();
+//   setBgImg(imgNumber);
+// }
 
-init();
+// init();
